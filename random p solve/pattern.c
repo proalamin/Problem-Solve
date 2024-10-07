fbef783d05adda0ad238fcbff121e01749b55615
proalamin <pro.alamin247@gmail.com>
2024-10-07 15:07:09 +0600

    pattern 3

--- random p solve/pattern.c
+++ random p solve/pattern.c
@@ -5,15 +5,24 @@ Pattern-1
  1 2 3
  1 2 3 4
  1 2 3 4 5
+ 
+ --------------------
 
 Pattern-2
  * 
  *  * 
  *  *  * 
  *  *  *  * 
- *  *  *  *  * 
+ *  *  *  *  *
  
- Pattern-3
+ --------------------
+
+Pattern-3
+ 1
+ 2 1
+ 3 2 1
+ 4 3 2 1
+ 5 4 3 2 1
 
 */
 
@@ -23,6 +32,7 @@ int main(){
 
 
     /* Pattern-1 */
+    printf("Pattern-1\n");
     for(int i=1; i<=5; i++){
         // printf("%d ", i);
 
@@ -35,11 +45,24 @@ int main(){
     printf("\n\n");
 
     /* Pattern-2 */
+    printf("Pattern-2\n");
     for(int i=1; i<=5; i++){
         // printf("%d ", i);
 
         for(int j=1; j<=i; j++){
             printf(" * ");
+        }
+        printf("\n");
+    }
+
+    printf("\n\n");
+
+    /* Pattern-3 */
+    printf("Pattern-3\n");
+    for(int i=1; i<=5; i++){
+
+        for(int j=i; j>=1; j--){
+            printf("%2d", j);
         }
         printf("\n");
     }
