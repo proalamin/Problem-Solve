1e10d61f5016875972c0f663494daeb64bcea42a
proalamin <pro.alamin247@gmail.com>
2024-10-31 19:50:04 +0600

    V - Frequency Array

---
+++ Online Judge/Codeforces/V_Frequency_Array.c
@@ -0,0 +1,27 @@
+/*
+https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/V
+*/
+
+#include<stdio.h>
+
+int f[100005];
+
+int main(){
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int a[n+5];
+    for(int i=0; i<n; i++){
+        scanf("%d", &a[i]);
+
+        f[a[i]] +=1;
+
+    }
+
+    for(int i=1; i<=m; i++){
+        printf("%d\n", f[i]);
+    }
+
+
+    return 0;
+}
