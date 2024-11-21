c4ade33bfb5b6d046ff1373b361eb43cd9027c2c
proalamin <pro.alamin247@gmail.com>
2024-11-21 16:27:43 +0600

    sorting - solve using sort() function

---
+++ Online Judge/Codeforces/H_Sorting.cpp
@@ -0,0 +1,26 @@
+/*
+https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/H
+*/
+// solve using sort() function
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>> n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>> arr[i];
+    }
+
+    sort(arr, arr+n);
+    for(int i=0; i<n; i++){
+        cout<< arr[i] << " ";
+    }
+
+
+    return 0;
+}
