d399cbfd2f0d16748b652401a122921f5fa790b5
proalamin <pro.alamin247@gmail.com>
2024-11-21 14:23:21 +0600

    find min and max value using min and max function

---
+++ Online Judge/Codeforces/K_Max_and_Min.cpp
@@ -0,0 +1,17 @@
+/*
+https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/K
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    cout << min(a, min(b,c))<< " " << max(a, max(b,c));
+
+
+    return 0;
+}
