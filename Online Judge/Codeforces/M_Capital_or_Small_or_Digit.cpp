a4cb5606f70161ae2683d39798b21bc5f8e4a4b1
proalamin <pro.alamin247@gmail.com>
2024-11-21 14:27:32 +0600

    M_Capital_or_Small_or_Digit.cpp

---
+++ Online Judge/Codeforces/M_Capital_or_Small_or_Digit.cpp
@@ -0,0 +1,23 @@
+/*https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/M*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    char x;
+    cin>> x;
+
+    if(x>= 48 && x<=57){
+        cout << "IS DIGIT" << endl;
+    }else{
+        cout << "ALPHA" << endl;
+        if(x>=65 && x<=90){
+            cout << "IS CAPITAL" << endl;
+        }else if(x>=97 && x<=122){
+            cout << "IS SMALL" << endl;
+        }
+    }
+
+    return 0;
+}
